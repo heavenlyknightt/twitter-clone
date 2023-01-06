45c43ddc80fce9dff5f1441d3f3d6edd4d5fcdfe
heavenlyknightt <99700560+heavenlyknightt@users.noreply.github.com>
2023-01-05 21:33:15 -0300

    Update index.tsx

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -9,6 +9,12 @@ import { Container, Wrapper } from './styles';
 
 const Layout: React.FC = () => {
   
+  const [username, setUsername] = useState('Natanael Martins'); 
+  const [bio, setBio] = useState('Estudante de Sistemas de Informação :)');
+  const [avatar, setAvatar] = useState<File | null>(null);
+  const [header, setHeader] = useState<File | null>(null);
+  const closeModal = () => setShowResults(false);
+  
   const [showEditPage, setShowEditPage] = useState(false);
   
   const handleEditClick = () => {
@@ -20,7 +26,18 @@ const Layout: React.FC = () => {
       <Wrapper>
         <MenuBar /> 
           <Main onEditClick={handleEditClick} />
-          { showEditPage ? <EditPage /> : null }
+          { showEditPage ? 
+          <EditPage
+              closeModal={closeModal}
+              username={username}
+              setUsername={setUsername}
+              bio={bio}
+              setBio={setBio}
+              avatar={avatar}
+              setAvatar={setAvatar}
+              header={header}
+              setHeader={setHeader}
+            /> : null }
         <SideBar />
       </Wrapper>
     </Container>
