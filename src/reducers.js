7fb373a1a8fc5d70eade184ef8ea512e9982aa0b
heavenlyknightt <99700560+heavenlyknightt@users.noreply.github.com>
2023-01-16 11:54:20 -0300

    Update reducers.js

--- src/reducers.js
+++ src/reducers.js
@@ -1,8 +1,46 @@
 import { combineReducers } from 'redux';
+
+const username = (state = 'Natanael Martins', action) => {
+  switch (action.type) {
+    case 'SET_USERNAME':
+      return action.username;
+    default:
+      return state;
+  }
+};
+
+const bio = (state = 'Estudante de Sistemas de Informação :)', action) => {
+  switch (action.type) {
+    case 'SET_BIO':
+      return action.bio;
+    default:
+      return state;
+  }
+};
+
+const avatar = (state = null, action) => {
+  switch (action.type) {
+    case 'SET_AVATAR':
+      return action.avatar;
+    default:
+      return state;
+  }
+};
+
+const header = (state = null, action) => {
+  switch (action.type) {
+    case 'SET_HEADER':
+      return action.header;
+    default:
+      return state;
+  }
+};
+
 const rootReducer = combineReducers({
   username,
   bio,
   avatar,
   header
 });
+
 export default rootReducer;
