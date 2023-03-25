92c20e9cf9492fde56641e06788f942df94bb281
heavenlyknightt <99700560+heavenlyknightt@users.noreply.github.com>
2023-03-25 09:03:31 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import { Provider } from 'react-redux';
+
 import GlobalStyles from './styles/GlobalStyles';
-import Layout from './components/Layout';
-import store from './store';
+
+import Layout from './components/Layout'
 
 function App() {
   return (
-    <Provider store={store}>
+    <>
       <Layout />
 
       <GlobalStyles />
-    </Provider>
+    </>
   );
 }
 
