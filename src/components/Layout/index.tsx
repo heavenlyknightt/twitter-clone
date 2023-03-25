1616c817e93f90312d40c710ce61965098e5af3e
heavenlyknightt <99700560+heavenlyknightt@users.noreply.github.com>
2023-03-25 08:44:46 -0300

    Update index.tsx

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,58 +1,25 @@
-/* import React, { useState } from 'react'; */
-import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { setUsername, setBio, setAvatar, setHeader } from './actions';
+import React, { createContext } from 'react';
 
 import MenuBar from '../MenuBar';
 import SideBar from '../SideBar';
 import Main from '../Main';
-import EditPage from '../EditPage';
 
 import { Container, Wrapper } from './styles';
 
+
+export const MessageContext = createContext("");
+
+const valor = "teste";
+
 const Layout: React.FC = () => {
-  
-  const username = useSelector((state) => state.user.username);
-  const bio = useSelector((state) => state.user.bio);
-  const avatar = useSelector((state) => state.user.avatar);
-  const header = useSelector((state) => state.user.header);
-  const modalOpen = useSelector((state) => state.modal.open);
-  const dispatch = useDispatch();
-
-  const handleEditClick = () => {
-    dispatch(setModalOpen(true));
-  }
-
-  const handleSave = (username, bio, avatar, header) => {
-    dispatch(setUsername(username));
-    dispatch(setBio(bio));
-    dispatch(setAvatar(avatar));
-    dispatch(setHeader(header));
-    dispatch(setModalOpen(false));
-  }
-  
   return (
     <Container>
       <Wrapper>
-        <MenuBar
-          username={username}
-          bio={bio}
-          avatar={avatar}
-          header={header}
-          /> 
-          <Main 
-            onEditClick={handleEditClick}
-            />
-          { modalOpen ? 
-          <EditPage
-            closeModal={() => dispatch(setModalOpen(false))}
-            username={username}
-            bio={bio}
-            avatar={avatar}
-            header={header}
-            onSave={handleSave}
-          /> : null }
-        <SideBar />
+        <MessageContext.Provider value={valor}>
+          <MenuBar />
+            <Main />
+          <SideBar />
+        </MessageContext.Provider>
       </Wrapper>
     </Container>
   );
