35cbe5d0e266c746cde188f858c2ce7cb0fb1ff5
heavenlyknightt <99700560+heavenlyknightt@users.noreply.github.com>
2023-03-25 21:41:20 -0300

    Update index.tsx

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -11,6 +11,8 @@ interface MessageContextInterface {
   setUsername: React.Dispatch<React.SetStateAction<string>>;
   bio: string;
   setBio: React.Dispatch<React.SetStateAction<string>>;
+  avatar: File | null | undefined;
+  setAvatar: React.Dispatch<React.SetStateAction<File>>;
 }
 
 export const MessageContext = React.createContext<MessageContextInterface>({
@@ -18,17 +20,23 @@ export const MessageContext = React.createContext<MessageContextInterface>({
   setUsername: () => {},
   bio: "",
   setBio: () => {},
+  avatar: null,
+  setAvatar: () => {},
+  header: null,
+  setHeader: () => {},
 });
 
 const Layout: React.FC = () => {
   
   const [username, setUsername] = useState("Natanael Martins");
   const [bio, setBio] = useState("Estudante de Sistemas de Informação :)");
+  const [avatar, setAvatar] = useState<File | null>(null);
+  const [header, setHeader] = useState<File | null>(null);
   
   return (
     <Container>
       <Wrapper>
-        <MessageContext.Provider value={{ username, setUsername, bio, setBio }}>
+        <MessageContext.Provider value={{ username, setUsername, bio, setBio, avatar, setAvatar, header, setHeader }}>
           <MenuBar />
             <Main />
           <SideBar />
